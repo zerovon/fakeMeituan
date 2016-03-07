cf37a7baa66535e5e576a1ffc7c7085df5c13e32
zerovon <zerovon316@gmail.com>
2016-03-07 23:56:41 +0800

    初始化项目

---
+++ 仿美团/仿美团/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿美团
+//
+//  Created by 冯钰洋 on 16/3/7.
+//  Copyright © 2016年 冯钰洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿美团/仿美团/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  仿美团
+//
+//  Created by 冯钰洋 on 16/3/7.
+//  Copyright © 2016年 冯钰洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
