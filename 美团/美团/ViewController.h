ffe01b536019f44eb6a35066f82bbba4cf83df1a
zerovon <zerovon316@gmail.com>
2016-03-08 01:46:26 +0800

    重新初始化

---
+++ 美团/美团/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  美团
+//
+//  Created by 冯钰洋 on 16/3/8.
+//  Copyright © 2016年 冯钰洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 美团/美团/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  美团
+//
+//  Created by 冯钰洋 on 16/3/8.
+//  Copyright © 2016年 冯钰洋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
